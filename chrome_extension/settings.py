e9320aad025346f28349081524d3ff8ef64c118a
remikeks <remikeks@gmail.com>
2023-10-02 10:31:41 +0100

    Change settings

--- chrome_extension/settings.py
+++ chrome_extension/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1hup(%v%gkdh!srrx+3rawh&vg7gzakd#a3#2p7+f)e=u+69%n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
